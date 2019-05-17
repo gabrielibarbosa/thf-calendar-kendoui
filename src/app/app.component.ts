a7354a8bdff486c4cf0ff2cce9754074090e8b84
Gabrieli Barbosa <ifc.gabrieli.barbosa@gmail.com>
2019-05-17 15:26:31 -0300

    Personalizando os botões

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+//import { SchedulerEvent } from '@progress/kendo-angular-scheduler';
+//import { sampleData, displayDate } from './events-utc';
+import { ThfMenuItem, ThfPageAction } from '@totvs/thf-ui';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'thf-calendar-kendoui';
+
+  public selectedViewIndex = 2;
+
+  readonly menus: Array<ThfMenuItem> = [
+    { label: 'Home', link: '/' },
+  ];
+
+  public readonly actions: Array<ThfPageAction> = [
+    { label: 'Novo' /*, action: this.add*/, icon: 'thf-icon-plus' }
+  ];
+
+  onClick($event) {
+    console.log($event);
+  }
+
+
+  //  public selectedDate: Date = displayDate;
+  //  public events: SchedulerEvent[] = sampleData;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,42 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { ChartModule } from '@progress/kendo-angular-charts';
+import { GridModule } from '@progress/kendo-angular-grid';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ThfKendoModule } from '@totvs/thf-kendo';
+
+
+import 'hammerjs';
+import { SchedulerModule } from '@progress/kendo-angular-scheduler';
+import { ThfModule } from '@totvs/thf-ui';
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonsModule,
+    ChartModule,
+    GridModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    SchedulerModule,
+    ThfModule,
+    ThfKendoModule,
+    RouterModule.forRoot([]),
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
